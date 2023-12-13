6e38887e10544d67a11fcdc0b9954427cc3fa909
fcuadros <fcuadros@bancolombia.com.co>
2023-12-12 20:34:01 -0500

    implementacion clientes sqs

--- src/main/java/edu/example/compratodosqs/ClientesSqsApplication.java
+++ src/main/java/edu/example/compratodosqs/ClientesSqsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CompraTodoSqsApplication {
+public class ClientesSqsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CompraTodoSqsApplication.class, args);
+		SpringApplication.run(ClientesSqsApplication.class, args);
 	}
 
 }
